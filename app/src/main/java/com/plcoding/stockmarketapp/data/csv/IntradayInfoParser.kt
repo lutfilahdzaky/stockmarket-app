26b52cfafbddc945e47c9914ead8499449ddce8f
Lutfilah Dzaky <lutfilahdzaky12@gmail.com>
2022-06-30 10:48:32 +0700

    Company info screen

--- app/src/main/java/com/plcoding/stockmarketapp/data/csv/IntradayInfoParser.kt
+++ app/src/main/java/com/plcoding/stockmarketapp/data/csv/IntradayInfoParser.kt
@@ -8,12 +8,12 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import java.io.InputStream
 import java.io.InputStreamReader
-import java.time.LocalDateTime
+import java.time.LocalDate
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class IntradayInfoParser @Inject constructor(): CSVParser<IntradayInfo> {
+class IntradayInfoParser @Inject constructor() : CSVParser<IntradayInfo> {
     override suspend fun parse(stream: InputStream): List<IntradayInfo> {
         val csvReader = CSVReader(InputStreamReader(stream))
         return withContext(Dispatchers.IO) {
@@ -27,7 +27,7 @@ class IntradayInfoParser @Inject constructor(): CSVParser<IntradayInfo> {
                     dto.toIntradayInfo()
                 }
                 .filter {
-                    it.date.dayOfMonth == LocalDateTime.now().minusDays(1).dayOfMonth
+                    it.date.dayOfMonth == LocalDate.now().minusDays(1).dayOfMonth
                 }
                 .sortedBy {
                     it.date.hour
